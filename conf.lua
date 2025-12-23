88362f2eea58297e6338be54e89b75e8afbb4ab1
End <contact@endoftimee.tech>
2025-12-22 21:46:50 -0700

    feat!: change version

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
     t.title = "Enhanced Runner"
-    t.version = "11.4"
+    t.version = "11.0"
     t.window.width = 800
     t.window.height = 400
     t.window.resizable = false
